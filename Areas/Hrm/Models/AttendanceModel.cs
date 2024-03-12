918a6cb470f2bc0fd85366d311f7e9113138a357
Rahat <islamrahatul2019@gmail.com>
2024-03-12 15:47:15 +0600

    attendance active status done

--- Areas/Hrm/Models/AttendanceModel.cs
+++ Areas/Hrm/Models/AttendanceModel.cs
@@ -6,7 +6,7 @@
         public int desig_id { get; set; }
         public DateTime starttime { get; set; }
         public DateTime endtime { get; set; }
-        public int is_active { get; set; }
+        public bool is_active { get; set; }
         public string status { get; set; }
         public string remarks { get; set; }
     }
